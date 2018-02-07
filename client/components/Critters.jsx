c0afa0177807952b560310b28f7fd9317d0765e2
Karen MacPherson <karen@fullstackacademy.com>
2018-02-07 17:22:57 -0500

    forms start

---
+++ client/components/Critters.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Critters = props => {
+  return (
+    <div id="critters">
+      {
+        props.critters.map(critter => {
+          return (
+            <div key={critter.name}>
+              <h1>{critter.name}</h1>
+              <img src={critter.image} />
+            </div>
+          )
+        })
+      }
+    </div>
+  )
+}
+
+export default Critters;
